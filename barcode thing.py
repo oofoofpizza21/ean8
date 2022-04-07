dfef34159c39d66fb29196cc7b6d6fa2fe51ef10
oofoofpizza21 <86003117+oofoofpizza21@users.noreply.github.com>
2022-04-07 13:30:37 +0200

    Barcode

---
+++ barcode thing.py
@@ -0,0 +1,49 @@
+no1 = int(input("Enter Number:"))
+no2 = int(input("Enter Number:"))
+no3 = int(input("Enter Number:"))
+no4 = int(input("Enter Number:"))
+no5 = int(input("Enter Number:"))
+no6 = int(input("Enter Number:"))
+no7 = int(input("Enter Number:"))
+
+sum1 = no1 + no3 + no5 + no7
+sum2 = no2 + no4 + no6
+sum4 = 3*sum1
+sum3 = sum4 + sum2
+add1 = sum3+1
+add2 = sum3+2
+add3 = sum3+3
+add4 = sum3+4
+add5 = sum3+5
+add6 = sum3+6
+add7 = sum3+7
+add8 = sum3+8
+add9 = sum3+9
+
+print (sum2 + sum4)
+
+if(sum3%10==0):
+    print("8th digit is a 0")
+if(add1%10==0):
+    print("8th digit is a 1")
+if(add2%10==0):
+    print("8th digit is a 2")
+if(add3%10==0):
+    print("8th digit is a 3")
+if(add4%10==0):
+    print("8th digit is a 4")
+if(add5%10==0):
+    print("8th digit is a 5")
+if(add6%10==0):
+    print("8th digit is a 6")
+if(add7%10==0):
+    print("8th digit is a 7")
+if(add8%10==0):
+    print("8th digit is a 8")
+if(add9%10==0):
+    print("8th digit is a 9")
+
+
+    
+
+    
